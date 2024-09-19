fcfaef38dca4b26fca394ffb6a666b608c9df0eb
xingyanjintian <xingyanjintian@gmail.com>
2024-09-19 23:24:44 +0900

    first commit

---
+++ src/基礎学習/BasicLearning.java
@@ -0,0 +1,15 @@
+package 基礎学習;
+
+public class BasicLearning {
+	public static void main(String[] args) {
+		double rand = Math.random()*100+1;
+		int number = (int)rand;
+		System.out.println(rand);
+		System.out.println(number);
+		//System.out.println("Hello world");
+
+		int num = 100+20;
+		System.out.println((num+9+5)*200/number/10);
+	}
+
+}
