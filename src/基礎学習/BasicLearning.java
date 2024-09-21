4c3e556f01c59d5cd276f3f9baada8cbd697313e
xingyanjintian <xingyanjintian@gmail.com>
2024-09-21 13:22:21 +0900

    if文練習用に変数numberを作成

--- src/基礎学習/BasicLearning.java
+++ src/基礎学習/BasicLearning.java
@@ -3,6 +3,15 @@ package 基礎学習;
 public class BasicLearning {
 	public static void main(String[] args) {
 		System.out.println("初めてのjava\n初めてのgithub");
+
+		//if文
+		int number = 1;//変数numberを作成
+		if(number==1){
+			System.out.println(number*100+"点");//numberが１の場合の処理
+		}else{
+			System.out.println("間違いです");//numberが１以外の処理
+			System.out.println("修正してください");
+		}
 	}
 
 }
