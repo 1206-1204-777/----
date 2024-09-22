f247450b2d6dbc893a0e4da7e29c0aea972f09d2
xingyanjintian <xingyanjintian@gmail.com>
2024-09-22 16:21:39 +0900

    変数numberを2に変更

--- src/基礎学習/BasicLearning.java
+++ src/基礎学習/BasicLearning.java
@@ -6,7 +6,7 @@ public class BasicLearning {
 
 		//if文
 		int number = 1;//変数numberを作成
-		if(number==1){
+		if(number==2){
 			System.out.println(number*100+"点");//numberが１の場合の処理
 		}else{
 			System.out.println("間違いです");//numberが１以外の処理
