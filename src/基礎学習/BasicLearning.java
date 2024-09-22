7638f4fac82150e510bfdbc4689a460e2d82291b
xingyanjintian <xingyanjintian@gmail.com>
2024-09-22 16:45:06 +0900

    if文をswith分に変更

--- src/基礎学習/BasicLearning.java
+++ src/基礎学習/BasicLearning.java
@@ -5,13 +5,11 @@ public class BasicLearning {
 		System.out.println("初めてのjava\n初めてのgithub");
 
 		//if文
-		int number = 10;//変数numberを作成
-		if(number==1){
-			System.out.println(number*100+"点");//numberが１の場合の処理
-		}else if(number==2){
-			System.out.println(number*100+"点");//numberが2の処理
-		}else{
-			System.out.println("修正してください");
-		}
+		int number = 1;//変数numberを作成
+            switch (number) {
+                case 1 -> System.out.println(number*100+"点");//numberが１の場合の処理
+                case 2 -> System.out.println(number*100+"点");//numberが2の処理
+                default -> System.out.println("修正してください");
+            }
 	}
 }
