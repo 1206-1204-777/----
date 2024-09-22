703ca4733e5a10b22fbffed1b201731004d86626
xingyanjintian <xingyanjintian@gmail.com>
2024-09-22 16:36:54 +0900

    if elseを追加しnumber変数の処理を変更

--- src/基礎学習/BasicLearning.java
+++ src/基礎学習/BasicLearning.java
@@ -5,11 +5,12 @@ public class BasicLearning {
 		System.out.println("初めてのjava\n初めてのgithub");
 
 		//if文
-		int number = 1;//変数numberを作成
-		if(number==2){
+		int number = 10;//変数numberを作成
+		if(number==1){
 			System.out.println(number*100+"点");//numberが１の場合の処理
+		}else if(number==2){
+			System.out.println(number*100+"点");//numberが2の処理
 		}else{
-			System.out.println("間違いです");//numberが１以外の処理
 			System.out.println("修正してください");
 		}
 	}
