e88bfa8b1850a42e510dc888f18c573bd1150174
xingyanjintian <xingyanjintian@gmail.com>
2024-09-23 13:08:17 +0900

    numberを1~３の乱数に変更

--- src/基礎学習/BasicLearning.java
+++ src/基礎学習/BasicLearning.java
@@ -1,11 +1,12 @@
 package 基礎学習;
-
+import java.util.Random;//乱数(整数)をインポート
 public class BasicLearning {
 	public static void main(String[] args) {
 		System.out.println("初めてのjava\n初めてのgithub");
 
-		//if文
-		int number = 1;//変数numberを作成
+		//switch文
+        Random rand = new Random();
+		int number = rand.nextInt(3)+1;//ランダム変数numberを作成 範囲１～３
             switch (number) {
                 case 1 -> System.out.println(number*100+"点");//numberが１の場合の処理
                 case 2 -> System.out.println(number*100+"点");//numberが2の処理
